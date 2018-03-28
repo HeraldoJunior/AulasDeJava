3c579dac588e8b92472b9cddf9a92076aa747d00
JuniorHeraldo <30849655+HeraldoJunior@users.noreply.github.com>
2018-03-28 20:31:26 -0300

    Adicionando novas opções no menu
    adicionado metodo de remoção por posição e alteração por posição

--- Pessoa/src/br/opet/tds172a/maintestesdiversos/MainComMenu.java
+++ Pessoa/src/br/opet/tds172a/maintestesdiversos/MainComMenu.java
@@ -26,16 +26,29 @@ public class MainComMenu {
 	 * constante é um valor que nunca irá se alterar por hipotese alguma
 	 */
 	// public static final int TAMANHO_LISTA_FUNCIONARIOS = 2;
-	
+
+	/**
+	 * OPÇÕES DECLARADAS POR MEIO DE CONSTANTE PARA FACILITAR O ENTENDIMENTO DO
+	 * CODIGO
+	 */
+	public static final int OPCAO_ADICIONAR = 1;
+	public static final int OPCAO_LISTAR = 2;
+	public static final int OPCAO_ALTERAR = 3;
+	public static final int OPCAO_REMOVER = 4;
+	public static final int OPCAO_SAIR = 9;
+
+	public int alteracaoFuncionario;
+
 	public static void main(String[] args) throws Exception {
 
 		/**
 		 * Arraylist para listagem de funcionarios
 		 */
 		List<Funcionario> listaDeFuncionarios = new ArrayList<Funcionario>();
-		
+
 		/**
-		 * Formatação da data para utilizar do mesmo padrão em mais ocasiões caso necessario
+		 * Formatação da data para utilizar do mesmo padrão em mais ocasiões caso
+		 * necessario
 		 * 
 		 */
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -63,8 +76,9 @@ public class MainComMenu {
 			System.out.println("Bem Vindo Ao Menu");
 			System.out.println("Digite a opção desejada");
 			System.out.println("1 - Adicionar Funcionario");
-			System.out.println("2 - Listar Funcionario");
-			System.out.println("3 - Remover todos os Funcionarios");
+			System.out.println("2 - Listar Funcionarios");
+			System.out.println("3 - Alterar Funcionario(Somente Nome e Data de Nascimento)");
+			System.out.println("4 - Remover Funcionario pela Matricula");
 			System.out.println("9 - Sair do Sistema");
 
 			/**
@@ -82,7 +96,7 @@ public class MainComMenu {
 			 * do processo com mensagens amigaveis.
 			 * 
 			 */
-			case 1:
+			case OPCAO_ADICIONAR:
 
 				/**
 				 * pergunta para o usuario inserir a matricula do funcionario a ser cadastrado
@@ -117,34 +131,21 @@ public class MainComMenu {
 				 */
 				Funcionario novoFuncionario = new Funcionario(nome, dataNascimento, matricula);
 				/**
-				 * inserção de um funcionario na lista por meio da criação do objeto com "novoFuncionario"
+				 * inserção de um funcionario na lista por meio da criação do objeto com
+				 * "novoFuncionario"
 				 */
 				listaDeFuncionarios.add(novoFuncionario);
 
-				/**
-				 * leitura do que for inserido na console para armazenar o nome do usuario
-				 */
-				System.out.println("Cadastro do funcionario concluido com sucesso!");
-				/**
-				 * lista de funcionarios recebendo o funcionario novo e o contador incrementando
-				 * 1 posição para não sobrescrever
-				 */
-				// listaDeFuncionarios[contador++] = funcionarioNovo;
-
-				/**
-				 * Opção de listar os funcionarios existentes
-				 */
-
 				break;
 
-			case 2: {
+			case OPCAO_LISTAR: {
 				/**
-				 * Opção para listar os funcionarios criados 
+				 * Opção para listar os funcionarios criados
 				 * 
 				 * Arrumar a formatação da data.
 				 * 
 				 */
-				if(listaDeFuncionarios.size() == 0) {
+				if (listaDeFuncionarios.size() == 0) {
 					System.out.println("Não existem funcionarios cadastrados");
 					break;
 				}
@@ -152,53 +153,90 @@ public class MainComMenu {
 				System.out.println("Nomes Imprindo a Lista com Array List");
 				for (int i = 0; i < listaDeFuncionarios.size(); i++) {
 					Funcionario funcionarioRecuperado = listaDeFuncionarios.get(i);
-					
+
 					System.out.println(
 							"Nome.: " + funcionarioRecuperado.nome + "- Matricula.: " + funcionarioRecuperado.matricula
 									+ " - Data de Nascimento.: " + sdf.format(funcionarioRecuperado.dataNascimento));
 				}
 
-				
-				// usando lista
-				// System.out.println("Lista Funcionarios: ");
-				// System.out.println("--------------------------------");
-				// System.out.println("Data de Nascimento - Nome ");
-
-				// for (int j = 0; j < listaFuncionarios.length; j++) {
-				// System.out.println(listaFuncionarios[j].getNome() + " - " +
-				// listaFuncionarios[j].getMatricula()
-				// + " - " + sdf.format(listaFuncionarios[j].getDataNascimento()));
-				// }
-
-				/**
-				 * Para casos onde tenha posições vazias dentro da lista
-				 * 
-				 */
-
-				// for (int j = 0; j < listaFuncionarios.length; j++) {
-
-				// if(listaFuncionarios[j] != null){
-				// System.out.println(listaFuncionarios[j].getNome() + " - "
-				// +listaFuncionarios[j].getMatricula() + " - " +
-				// sdf.format(listaFuncionarios[j].getDataNascimento())); } }
 				break;
 			}
-			
-			case 3:
-				
-				for (int i = listaDeFuncionarios.size() - 1; i >= 0; i--) {
-					listaDeFuncionarios.remove(i);
+			/**
+			 * Opção de alterar o funcionario
+			 */
+			case OPCAO_ALTERAR:
+				System.out.println("Insira a matricula do funcionario a ser removido");
+				int matriculaProcurada = MyReader.readInt();
+
+				for (int i = 0; i < listaDeFuncionarios.size(); i++) {
 					
-					System.out.println("Funcionario " + i + " Excluidos com sucesso!");
+					if (matriculaProcurada == listaDeFuncionarios.get(i).getMatricula()) {
 
+						/**
+						 * pergunta para o usuario inserir o nome do funcionario a ser cadastrado
+						 */
+						System.out.println("Insira a Nome do funcionario e aperte enter!");
+						/**
+						 * leitura do que for inserido na console para armazenar o nome do funcionario
+						 */
+						nome = MyReader.readString();
+
+						/**
+						 * pergunta para o usuario inserir o nome do funcionario a ser cadastrado
+						 */
+						System.out.println("Insira a Data de Nascimento do funcionario e aperte enter!");
+						/**
+						 * leitura do que for inserido na console para armazenar a data de nascimento do
+						 * funcionario
+						 */
+						dataNascimento = MyReader.readDate();
+						/**
+						 * get e set do nome da data de nascimento para alterar o cadastro do funcionario
+						 */
+						listaDeFuncionarios.get(i).setNome(nome);
+						listaDeFuncionarios.get(i).setDataNascimento(dataNascimento);
+						System.out.println("Alterado com sucesso!");
+
+					}else {
+						System.out.println("Não Alterado!");
+
+					}
 				}
+				break;
 
-			case 9:
+			case OPCAO_REMOVER:
+
+				System.out.println("Insira a matricula do funcionario a ser removido");
+				matriculaProcurada = MyReader.readInt();
+
+				for (int i = 0; i < listaDeFuncionarios.size(); i++) {
+					if (matriculaProcurada == listaDeFuncionarios.get(i).getMatricula()) {
+
+						System.out.println("Dados do funcionário com a matricula " + matriculaProcurada + ":\nNome: "
+								+ listaDeFuncionarios.get(i).getNome() + " - Data de nascimento: "
+								+ sdf.format(listaDeFuncionarios.get(i).getDataNascimento()));
+						System.out.println("Deseja remover o funcionario da matricula " + matriculaProcurada
+								+ "? (Digite 1 parasim ou 2 para não");
+						int opcaoEscolhida = MyReader.readInt();
+						if (opcaoEscolhida == 1) {
+							listaDeFuncionarios.remove(i);
+							System.out.println("Funcionário removido com sucesso");
+
+						}else {
+							System.out.println("Funcionario não removido!");
+						}
+					}
+				}
+				break;
+
+			case OPCAO_SAIR:
 				System.out.println("--------------------------------");
+
 				System.out.println("Obrigado por utilizar o Sistema!");
 				System.out.println("Até a Proxima!");
 
 				System.out.println("--------------------------------");
+
 				break;
 			/**
 			 * Opção para caso alguma outra tecla seja in incorreta seja inserida, mensagem
@@ -211,7 +249,7 @@ public class MainComMenu {
 			/**
 			 * final do looping informando quando o memso irá ser finalizado
 			 */
-		} while (menu < 9);
+		} while (menu < OPCAO_SAIR);
 
 		/**
 		 * Mensagens finais do sistema para informar o fim da execução
