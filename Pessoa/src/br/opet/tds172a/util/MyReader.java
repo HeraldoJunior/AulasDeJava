9b387bbea21caded49e1656eb916d987a205be5d
JuniorHeraldo <30849655+HeraldoJunior@users.noreply.github.com>
2018-04-08 02:24:56 -0300

    Adicionado um reader proprio
    adicionado reader para auxiliar nas demais utilizações do sistema

--- Pessoa/src/br/opet/tds172a/util/MyReader.java
+++ Pessoa/src/br/opet/tds172a/util/MyReader.java
@@ -7,66 +7,83 @@ import java.util.Scanner;
 public class MyReader {
 
 	private static Scanner scanner = new Scanner(System.in);
-	
-	/**
-	 * Método de leitura com a classe scanner de uma variavel String digitada pelo console
-	 * @return
-	 */
-	public static final String readString(){
-        String text = scanner.nextLine();
-        return text;
-    }
 
 	/**
-	 * Método de leitura com a classe scanner de uma variavel Int digitada pelo console
+	 * Método de leitura com a classe scanner de uma variavel String digitada pelo
+	 * console
+	 * 
 	 * @return
 	 */
-    public static final int readInt() throws Exception{
-        String i = scanner.nextLine();
-        int ii = 0;
-        try{
-            ii = Integer.parseInt(i);
-        }catch(Exception e){
-            System.out.println("Não foi possível transformar: " + i + " em Inteiro");
-            throw e;
-        }
-        return ii;
-    }
-
-    /**
-	 * Método de leitura com a classe scanner de uma variavel Double digitada pelo console
-	 * @return
-	 */
-    public static final double readDouble() throws Exception{
-        String db = scanner.nextLine();
-        double dd = 0;
-        try{
-            dd = Double.parseDouble(db);
-        }catch(Exception e){
-            System.out.println("Não foi possível transformar: " + db + " em Double");
-            throw e;
-        }
-        return dd;
-    }
-
-    /**
-	 * Método de leitura com a classe scanner de uma variavel Boolean digitada pelo console
-	 * @return
-	 */
-    public static final boolean readBoolean() throws Exception{
-        String b = scanner.nextLine();
-        boolean bb = false;
-        try{
-            bb = Boolean.parseBoolean(b);
-        }catch(Exception e){
-            System.out.println("Não foi possível transformar: " + b + " em Boolean");
-            throw e;
-        }
-        return bb;
-    }
+	public static final String readString() {
+		String text = scanner.nextLine();
+		return text;
+	}
 
 	/**
-	 * Função criada para retornar a data no formato DATE quando é inserida no formato STRING
+	 * Método de leitura com a classe scanner de uma variavel Int digitada pelo
+	 * console
+	 * 
+	 * @return
+	 */
+	public static final int readInt() {
+
+		boolean validou = false;
+		int ii = 0;
+
+		do {
+			String i = scanner.nextLine();
+			try {
+				ii = Integer.parseInt(i);
+				validou = true;
+			} catch (Exception e) {
+				System.out.println("Não foi possível transformar: " + i + " em Inteiro");
+				validou = false;
+			}
+		} while (!validou);
+
+		return ii;
+	}
+
+	/**
+	 * Método de leitura com a classe scanner de uma variavel Double digitada pelo
+	 * console
+	 * 
+	 * @return
+	 */
+	public static final double readDouble() throws Exception {
+		String db = scanner.nextLine();
+		double dd = 0;
+		try {
+			dd = Double.parseDouble(db);
+		} catch (Exception e) {
+			System.out.println("Não foi possível transformar: " + db + " em Double");
+			throw e;
+		}
+		return dd;
+	}
+
+	/**
+	 * Método de leitura com a classe scanner de uma variavel Boolean digitada pelo
+	 * console
+	 * 
+	 * @return
+	 */
+	public static final boolean readBoolean() throws Exception {
+		String b = scanner.nextLine();
+		boolean bb = false;
+		try {
+			bb = Boolean.parseBoolean(b);
+		} catch (Exception e) {
+			System.out.println("Não foi possível transformar: " + b + " em Boolean");
+			throw e;
+		}
+		return bb;
+	}
+
+	/**
+	 * Função criada para retornar a data no formato DATE quando é inserida no
+	 * formato STRING
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
